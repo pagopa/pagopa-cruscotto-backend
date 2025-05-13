83e3fc75edea9a9a48e08eaefd3029e4d0cc9104
Andrea Maria Regalli <a.regalli@reply.it>
2025-05-13 09:19:58 +0200

    validazione ShutdownRequestBean

--- src/main/java/com/nexigroup/pagopa/cruscotto/service/bean/ShutdownRequestBean.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/service/bean/ShutdownRequestBean.java
@@ -1,5 +1,6 @@
 package com.nexigroup.pagopa.cruscotto.service.bean;
 
+import com.nexigroup.pagopa.cruscotto.service.validation.ValidRangeDate;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -10,6 +11,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@ValidRangeDate(minDate = "shutdownStartDate", maxDate = "shutdownEndDate", pattern = "dd/MM/yyyy", field = "FIELD@shutdownPeriodStartDateEndDate")
 public class ShutdownRequestBean {
 
     private Long id;
