c823eedd839d026f3bab3eb1aae5e5f4b236a6e8
D00B210 <matteo.tofoni@nexigroup.com>
2025-05-22 15:42:58 +0200

    fix instance @NotNull

--- src/main/java/com/nexigroup/pagopa/cruscotto/domain/Instance.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/domain/Instance.java
@@ -85,11 +85,9 @@ public class Instance extends AbstractAuditingEntity<Long> implements Serializab
     @Column(name = "TE_STATUS", nullable = false)
     private InstanceStatus status;
 
-    @NotNull
     @Column(name = "DT_LAST_ANALISYS_DATE")
     private Instant lastAnalysisDate;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(name = "TE_LAST_ANALISYS_OUTCOME")
     private AnalysisOutcome lastAnalysisOutcome;
