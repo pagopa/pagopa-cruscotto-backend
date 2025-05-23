ccf96e00e30e73c6d4cae2b3f39c6a0353b46636
D00B210 <matteo.tofoni@nexigroup.com>
2025-05-23 16:26:42 +0200

    add json output console appender + filter rest service partner

--- src/main/java/com/nexigroup/pagopa/cruscotto/service/AnagPartnerService.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/service/AnagPartnerService.java
@@ -1,11 +1,7 @@
 package com.nexigroup.pagopa.cruscotto.service;
 
 import com.nexigroup.pagopa.cruscotto.domain.AnagPartner;
-import com.nexigroup.pagopa.cruscotto.domain.Instance;
 import com.nexigroup.pagopa.cruscotto.service.dto.AnagPartnerDTO;
-import com.nexigroup.pagopa.cruscotto.service.dto.InstanceDTO;
-import com.nexigroup.pagopa.cruscotto.service.dto.TaxonomyDTO;
-import com.nexigroup.pagopa.cruscotto.service.filter.InstanceFilter;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,10 +13,11 @@ public interface AnagPartnerService {
     /**
      * Get all the partner.
      *
-     * @param pageable the pagination information.
+     * @param nameFilter filter by partner name.
+     * @param pageable   the pagination information.
      * @return the list of entities.
      */
-    Page<AnagPartnerDTO> findAll(Pageable pageable);
+    Page<AnagPartnerDTO> findAll(String nameFilter, Pageable pageable);
 
     /**
      * Save all partners.
