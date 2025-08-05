17ad7bb720cca06d131ebc7766e63285df3f6585
alfonso-iazzetti <alfonso.iazzetti@accenture.com>
2025-08-05 16:45:01 +0200

    added api to update instanceModule

--- src/main/java/com/nexigroup/pagopa/cruscotto/web/rest/InstanceModuleResource.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/web/rest/InstanceModuleResource.java
@@ -1,21 +1,25 @@
 package com.nexigroup.pagopa.cruscotto.web.rest;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nexigroup.pagopa.cruscotto.domain.AuthUser;
 import com.nexigroup.pagopa.cruscotto.domain.InstanceModule;
 import com.nexigroup.pagopa.cruscotto.security.AuthoritiesConstants;
 import com.nexigroup.pagopa.cruscotto.service.InstanceModuleService;
 import com.nexigroup.pagopa.cruscotto.service.dto.InstanceModuleDTO;
-
-import java.util.Optional;
+import com.nexigroup.pagopa.cruscotto.service.util.UserUtils;
 
 /**
  * REST controller for managing {@link InstanceModule}.
@@ -33,9 +37,12 @@ public class InstanceModuleResource {
 
     private final InstanceModuleService instanceModuleService;
     
+    private final UserUtils userUtils;
+    
 
-    public InstanceModuleResource(InstanceModuleService instanceModuleService) {
+    public InstanceModuleResource(InstanceModuleService instanceModuleService, UserUtils userUtils) {
         this.instanceModuleService = instanceModuleService;
+        this.userUtils = userUtils;
     }
 
     /**
@@ -51,5 +58,41 @@ public class InstanceModuleResource {
         log.debug("REST request per ottenere InstanceModule con id : {}", id);
         Optional<InstanceModuleDTO> instanceModuleDTO = instanceModuleService.findInstanceModuleDTOById(id);
         return instanceModuleDTO.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    /*
+    @PatchMapping ("/instance-modules/{id}")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.INSTANCE_MODULE_DETAIL + "\")") //TODO USE INSTANCE_MODULE_MODIFICATION
+    public ResponseEntity<Void> updateInstanceModule(@PathVariable Long id, @RequestBody InstanceModuleUpdateStatusDTO body) {
+    	instanceModuleService.updateStatusAndAllowManualOutcome(id, body.getStatus(), body.getAllowManualOutcome());
+    	return ResponseEntity.ok().build();
+    	
+    }*/
+    
+    /**
+     * {@code PATCH /instance-modules/:id} : Updates an existing InstanceModule.
+     *
+     * @param id the ID of the InstanceModule to update
+     * @param body the InstanceModuleDTO containing the fields to update
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)}
+     */
+    @PatchMapping ("/instance-modules")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.INSTANCE_MODULE_DETAIL + "\")") //TODO USE INSTANCE_MODULE_MODIFICATION
+    public ResponseEntity<InstanceModuleDTO> updateInstanceModule(@RequestBody InstanceModuleDTO body) {
+        log.debug("REST request to update InstanceModule with id: {}", body.getId());
+        
+        // Get current authenticated user information
+        AuthUser currentUser = userUtils.getLoggedUser();
+        log.debug("User '{}' (ID: {}, Name: {} {}) is updating InstanceModule with id: {}", 
+                currentUser.getLogin(), 
+                currentUser.getId(),
+                currentUser.getFirstName(), 
+                currentUser.getLastName(), 
+                body.getId());
+        
+        
+        InstanceModuleDTO result = instanceModuleService.updateInstanceModule(body, currentUser);
+        
+        log.info("InstanceModule with id: {} successfully updated by user: {}", body.getId(), currentUser.getLogin());
+        return ResponseEntity.ok(result);
     }
 }
