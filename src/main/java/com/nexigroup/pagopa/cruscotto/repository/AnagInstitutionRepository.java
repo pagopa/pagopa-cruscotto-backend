bd0d9bd3fa23b96c4b275d04b7f4b831a88b95e3
alfonso-iazzetti <alfonso.iazzetti@accenture.com>
2025-08-07 20:09:24 +0200

    rollback findByFiscalCode

--- src/main/java/com/nexigroup/pagopa/cruscotto/repository/AnagInstitutionRepository.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/repository/AnagInstitutionRepository.java
@@ -1,7 +1,5 @@
 package com.nexigroup.pagopa.cruscotto.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +7,5 @@ import com.nexigroup.pagopa.cruscotto.domain.AnagInstitution;
 
 @Repository
 public interface AnagInstitutionRepository extends JpaRepository<AnagInstitution, Long> {
-    List<AnagInstitution> findByFiscalCode(String fiscalCode);
+    AnagInstitution findByFiscalCode(String fiscalCode);
 }
