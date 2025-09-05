941925bdfe462c3d64c254f08fe11563b2511593
marimpietri-acn <alberto.marimpietri@accenture.com>
2025-09-05 11:52:29 +0200

    incorrect taxonomy data saved in the analysis instance

---
+++ src/main/java/com/nexigroup/pagopa/cruscotto/service/KpiA2AnalyticIncorrectTaxonomyDataService.java
@@ -0,0 +1,10 @@
+package com.nexigroup.pagopa.cruscotto.service;
+
+import com.nexigroup.pagopa.cruscotto.service.dto.KpiA2AnalyticIncorrectTaxonomyDataDTO;
+import java.util.List;
+
+public interface KpiA2AnalyticIncorrectTaxonomyDataService {
+    void saveAll(List<KpiA2AnalyticIncorrectTaxonomyDataDTO> incorrectTaxonomyDataList);
+
+    List<KpiA2AnalyticIncorrectTaxonomyDataDTO> findByKpiA2AnalyticDataId(Long analyticDataId);
+}
