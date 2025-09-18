67d95daebf5c6ddc65784d6a1e53ec8b47220ffe
marimpietri-acn <alberto.marimpietri@accenture.com>
2025-09-18 11:52:49 +0200

    drilldown table liquibase update

--- src/main/java/com/nexigroup/pagopa/cruscotto/web/rest/KpiA1AnalyticDrillDownResource.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/web/rest/KpiA1AnalyticDrillDownResource.java
@@ -3,7 +3,11 @@ package com.nexigroup.pagopa.cruscotto.web.rest;
 import com.nexigroup.pagopa.cruscotto.service.KpiA1AnalyticDrillDownService;
 import com.nexigroup.pagopa.cruscotto.service.dto.KpiA1AnalyticDrillDownDTO;
 import java.util.List;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
+import tech.jhipster.web.util.ResponseUtil;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +23,10 @@ public class KpiA1AnalyticDrillDownResource {
     @GetMapping("/{analyticDataId}")
     public ResponseEntity<List<KpiA1AnalyticDrillDownDTO>> getByAnalyticDataId(@PathVariable Long analyticDataId) {
         List<KpiA1AnalyticDrillDownDTO> result = drillDownService.findByKpiA1AnalyticDataId(analyticDataId);
-        return ResponseEntity.ok(result);
+        
+        return ResponseUtil.wrapOrNotFound(
+            Optional.ofNullable(result == null || result.isEmpty() ? null : result)
+        );
+        
     }
 }
