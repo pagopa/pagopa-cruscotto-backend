62ac4b1958b84ad7ecf8341865f2eb7eb78ad4a0
CO0B686 <alberto.marimpietri@external.nexigoup.com>
2025-09-23 17:27:27 +0200

    release fix 23-sett

--- src/main/java/com/nexigroup/pagopa/cruscotto/service/impl/KpiA1AnalyticDrillDownServiceImpl.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/service/impl/KpiA1AnalyticDrillDownServiceImpl.java
@@ -8,9 +8,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class KpiA1AnalyticDrillDownServiceImpl implements KpiA1AnalyticDrillDownService {
     @Override
     public void deleteByKpiA1AnalyticDataIds(List<Long> analyticDataIds) {
