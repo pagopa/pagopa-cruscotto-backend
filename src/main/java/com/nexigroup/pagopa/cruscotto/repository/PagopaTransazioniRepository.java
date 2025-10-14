3883c78058f057c93d945907a1d3cfaa5d97dad1
CO0B686 <alberto.marimpietri@external.nexigoup.com>
2025-10-14 14:34:32 +0200

    app liquibase version conf

--- src/main/java/com/nexigroup/pagopa/cruscotto/repository/PagopaTransazioniRepository.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/repository/PagopaTransazioniRepository.java
@@ -15,40 +15,40 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PagopaTransazioniRepository extends JpaRepository<PagopaTransaction, Long>, JpaSpecificationExecutor<PagopaTransaction> {
     
-    @Query("SELECT p FROM PagopaTransazioni p WHERE p.data BETWEEN :startDate AND :endDate")
+    @Query("SELECT p FROM PagopaTransaction p WHERE p.date BETWEEN :startDate AND :endDate")
     List<PagopaTransaction> findByDateRange(
         @Param("startDate") LocalDate startDate,
         @Param("endDate") LocalDate endDate
     );
 
-    @Query("SELECT DISTINCT p.partner FROM PagopaTransazioni p WHERE p.data BETWEEN :startDate AND :endDate")
+    @Query("SELECT DISTINCT p.cfPartner FROM PagopaTransaction p WHERE p.date BETWEEN :startDate AND :endDate")
     List<String> findDistinctPartnersByDateRange(
         @Param("startDate") LocalDate startDate,
         @Param("endDate") LocalDate endDate
     );
 
-    @Query("SELECT COUNT(DISTINCT p.ente) FROM PagopaTransazioni p WHERE p.partner = :partner AND p.data BETWEEN :startDate AND :endDate")
+    @Query("SELECT COUNT(DISTINCT p.cfInstitution) FROM PagopaTransaction p WHERE p.cfPartner = :partner AND p.date BETWEEN :startDate AND :endDate")
     Integer countDistinctEntitiesByPartnerAndDateRange(
         @Param("partner") String partner,
         @Param("startDate") LocalDate startDate,
         @Param("endDate") LocalDate endDate
     );
 
-    @Query("SELECT SUM(p.totaleTransazioni) FROM PagopaTransazioni p WHERE p.partner = :partner AND p.data BETWEEN :startDate AND :endDate")
+    @Query("SELECT SUM(p.transactionTotal) FROM PagopaTransaction p WHERE p.cfPartner = :partner AND p.date BETWEEN :startDate AND :endDate")
     Long countTransactionsByPartnerAndDateRange(
         @Param("partner") String partner,
         @Param("startDate") LocalDate startDate,
         @Param("endDate") LocalDate endDate
     );
 
-    @Query("SELECT p FROM PagopaTransazioni p WHERE p.partner = :partner AND p.data BETWEEN :startDate AND :endDate")
+    @Query("SELECT p FROM PagopaTransaction p WHERE p.cfPartner = :partner AND p.date BETWEEN :startDate AND :endDate")
     List<PagopaTransaction> findByPartnerAndDateRange(
         @Param("partner") String partner,
         @Param("startDate") LocalDate startDate,
         @Param("endDate") LocalDate endDate
     );
 
-    @Query("SELECT p FROM PagopaTransazioni p WHERE p.ente = :ente AND p.data BETWEEN :startDate AND :endDate")
+    @Query("SELECT p FROM PagopaTransaction p WHERE p.cfInstitution = :ente AND p.date BETWEEN :startDate AND :endDate")
     List<PagopaTransaction> findByEnteAndDateRange(
         @Param("ente") String ente,
         @Param("startDate") LocalDate startDate,
