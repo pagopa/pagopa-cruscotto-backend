03b25ede267421d0e4f3e1a7dae584468a9f1413
CO0B686 <alberto.marimpietri@external.nexigoup.com>
2025-10-21 12:08:02 +0200

    api_log sequence fix + permissions

--- src/main/java/com/nexigroup/pagopa/cruscotto/web/rest/KpiA1AnalyticDrillDownResource.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/web/rest/KpiA1AnalyticDrillDownResource.java
@@ -1,5 +1,6 @@
 package com.nexigroup.pagopa.cruscotto.web.rest;
 
+import com.nexigroup.pagopa.cruscotto.security.AuthoritiesConstants;
 import com.nexigroup.pagopa.cruscotto.service.KpiA1AnalyticDrillDownService;
 import com.nexigroup.pagopa.cruscotto.service.dto.KpiA1AnalyticDrillDownDTO;
 import java.util.List;
@@ -9,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import tech.jhipster.web.util.ResponseUtil;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,7 @@ public class KpiA1AnalyticDrillDownResource {
     private final KpiA1AnalyticDrillDownService drillDownService;
 
     @GetMapping("/{analyticDataId}")
+    @PreAuthorize("hasAuthority(\"" + AuthoritiesConstants.KPI_A1_ANALITIC_DATA_DETAIL + "\")")
     public ResponseEntity<List<KpiA1AnalyticDrillDownDTO>> getByAnalyticDataId(@PathVariable Long analyticDataId) {
         List<KpiA1AnalyticDrillDownDTO> result = drillDownService.findByKpiA1AnalyticDataId(analyticDataId);
         
