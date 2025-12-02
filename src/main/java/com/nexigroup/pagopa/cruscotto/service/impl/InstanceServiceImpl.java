6d59884c1eba035bbe1dc876c1ab2fd4ddd0d6c2
CO0B686 <alberto.marimpietri@external.nexigoup.com>
2025-12-02 14:21:07 +0100

    release 21nov modernizer

--- src/main/java/com/nexigroup/pagopa/cruscotto/service/impl/InstanceServiceImpl.java
+++ src/main/java/com/nexigroup/pagopa/cruscotto/service/impl/InstanceServiceImpl.java
@@ -439,8 +439,8 @@ public class InstanceServiceImpl implements InstanceService {
                                 && instance.getChangePartnerQualified())
                         .findFirst();
 
-                if (mostRecentChangedQualifiedInstance.isPresent()) {
-                    Instance instance = mostRecentChangedQualifiedInstance.get();
+                mostRecentChangedQualifiedInstance.ifPresentOrElse(
+                instance -> {
                     AnalysisOutcome outcome = instance.getLastAnalysisOutcome();
 
                     // Partner is qualified if outcome is OK
@@ -455,13 +455,15 @@ public class InstanceServiceImpl implements InstanceService {
                             isQualified,
                             instance.getInstanceIdentification(),
                             outcome);
-                } else {
+                },
+                () -> {
                     // No remaining instance changed qualification, restore to default qualified flag
                     anagPartnerService.changePartnerQualified(partnerId, false);
                     LOGGER.info(
                             "Partner {} qualified flag restored to default because no remaining instance changed qualification for partner {}.",
                             partnerId);
                 }
+            );
 
             } else {
                 // If changePartnerQualified is false or null, retain current qualified status
